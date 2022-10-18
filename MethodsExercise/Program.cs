83e1104a39c17a7056520a5de3d0bb33d17cc05d
clarence <cgwhite76@gmail.com>
2022-10-18 17:08:01 -0500

    Final Exercise1

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -1,10 +1,36 @@
-﻿namespace MethodsExercise
-{
+﻿//namespace MethodsExercise
+
     public class Program
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
+    static void Main(string[] args)
+
+    {
+        Console.WriteLine("What is your first name?");
+        var myName = Console.ReadLine();
+        Console.WriteLine("What is your favorite color?");
+        var myColor = Console.ReadLine();
+        Console.WriteLine("What is your favorite animal?");
+        var myAnimal = Console.ReadLine();
+        Console.WriteLine("What is your favorite band?");
+        var myBand = Console.ReadLine();
+        MyResult(myName, myColor, myAnimal, myBand);
     }
+    static void MyResult(string myName, string myColor, string myAnimal, string myBand)
+
+    {
+        var myStory = ($"This is a very short story about {myName}. ");
+        myStory = myStory + ($"Who loved his {myColor} {myAnimal}. ");
+        myStory = myStory + ($"And they both loved to listen to the {myBand}.");
+        Console.WriteLine(myStory);
+    }
+
+
+
+
+    
+
+
 }
+
+
+
